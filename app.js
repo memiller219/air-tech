89c001606067e2f981c0f20883514248b448f302
Michael Miller <memiller219@gmail.com>
2019-04-26 22:03:51 +0000

    heroku push

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.listen(process.env.PORT, process.env.IP);
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+var $ = require("jquery")(window);
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('our app is running');
+});
+
